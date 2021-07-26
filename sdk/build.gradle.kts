8e3b4fd253b2856f8536d38f23c1de103e28cde3
Wyatt <wbaker@figure.com>
2021-07-26 11:55:59 -0700

    Working on adding dehydrateSpec function

--- sdk/build.gradle.kts
+++ sdk/build.gradle.kts
@@ -4,7 +4,10 @@ dependencies {
     api(project(":contract-proto"))
 
     implementation(project(":encryption"))
+    implementation(project(":contract-proto"))
 
+    implementation("org.jetbrains.kotlin", "kotlin-reflect", Version.kotlin)
     implementation("com.google.protobuf", "protobuf-java", Version.protobuf)
+    implementation("com.google.protobuf", "protobuf-java-util", Version.protobuf)
     implementation("com.google.guava", "guava", Version.guava)
 }
