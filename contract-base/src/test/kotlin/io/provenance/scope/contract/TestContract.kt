83d659a479069f1e954f44a6d0dd29c1912523c2
Pierce Trey <ptrey@figure.com>
2021-10-01 17:05:57 -0600

    tests for Function class

--- contract-base/src/test/kotlin/io/provenance/scope/contract/TestContract.kt
+++ contract-base/src/test/kotlin/io/provenance/scope/contract/TestContract.kt
@@ -1,6 +1,7 @@
 package io.provenance.scope.contract
 
 import io.provenance.scope.contract.annotations.Function
+import io.provenance.scope.contract.annotations.Input
 import io.provenance.scope.contract.annotations.Participants
 import io.provenance.scope.contract.annotations.Record
 import io.provenance.scope.contract.annotations.ScopeSpecification
@@ -22,6 +23,49 @@ class TestContract(@Record(name = "testRecord") val testRecordValue: List<HelloW
     @Record(name = "testRecord")
     @Function(Specifications.PartyType.OWNER)
     fun printTest(@Record(name = "testRecord") testPrintValue: String) { println("TestRecordValue for $testPrintValue")}
+
+    @Record(name = "testRecordTwoInputs")
+    @Function(Specifications.PartyType.OWNER)
+    fun testRecordTwoInputs(
+        @Input(name = "testRecordInputOne") testRecordInputOne: TestContractProtos.TestProto,
+        @Input(name = "testRecordInputTwo") testRecordInputTwo: TestContractProtos.TestProto
+    ) = TestContractProtos.TestProto.newBuilder()
+        .setValue(testRecordInputOne.value + testRecordInputTwo.value)
+        .build()
+
+    @Record(name = "testRecordTwoRecords")
+    @Function(Specifications.PartyType.OWNER)
+    fun testRecordTwoRecords(
+        @Record(name = "testRecordOne") testRecordOne: TestContractProtos.TestProto,
+        @Record(name = "testRecordTwo") testRecordTwo: TestContractProtos.TestProto,
+    ) = TestContractProtos.TestProto.newBuilder()
+        .setValue(testRecordOne.value + testRecordTwo.value)
+        .build()
+
+    @Record(name = "testRecordOneInputOneRecord")
+    @Function(Specifications.PartyType.OWNER)
+    fun testRecordOneInputOneRecord(
+        @Record(name = "testRecordOne") testRecordOne: TestContractProtos.TestProto,
+        @Input(name = "testRecordInputOne") testRecordInputOne: TestContractProtos.TestProto,
+    ) = TestContractProtos.TestProto.newBuilder()
+        .setValue(testRecordOne.value + testRecordInputOne.value)
+        .build()
+
+    @Record(name = "testRecordNonAnnotatedArgument")
+    @Function(Specifications.PartyType.OWNER)
+    fun testRecordNonAnnotatedArgument(
+        testRecordOneNoAnnotation: TestContractProtos.TestProto,
+    ) = TestContractProtos.TestProto.newBuilder()
+        .setValue(testRecordOneNoAnnotation.value + "-modified")
+        .build()
+
+    @Record(name = "testRecordDoubleAnnotatedArgument")
+    @Function(Specifications.PartyType.OWNER)
+    fun testRecordDoubleAnnotatedArgument(
+        @Record(name = "testRecordOne") @Input(name = "testRecordInputOne") testRecordOneDoubleAnnotation: TestContractProtos.TestProto,
+    ) = TestContractProtos.TestProto.newBuilder()
+        .setValue(testRecordOneDoubleAnnotation.value + "-modified")
+        .build()
 }
 
 @ScopeSpecificationDefinition(
