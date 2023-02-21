fc6a8ba7b7b6fe984d183d5cdbe2d08b2a4079d3
arnabmitra <arnabmitra.mitra@gmail.com>
2023-02-21 14:03:58 -0700

    Updating protoc deps and grpc deps so that it compiles on m1 and also bring them upto latest.

--- buildSrc/src/main/kotlin/Version.kt
+++ buildSrc/src/main/kotlin/Version.kt
@@ -1,15 +1,15 @@
 object Version {
-    const val protobufPlugin = "0.8.16"
+    const val protobufPlugin = "0.8.19"
     const val nexusPublishPlugin = "1.1.0"
 
     const val kotlin = "1.4.31"
-    const val protobuf = "3.12.0"
-    const val provenanceProtos = "1.8.0-rc10"
+    const val protobuf = "3.21.12"
+    const val provenanceProtos = "1.13.1"
     const val guava = "30.1.1-jre"
     const val fortanixKms = "3.23.1408"
 
     const val bouncy_castle = "1.69"
-    const val grpc_version = "1.36.2"
+    const val grpc_version = "1.51.1"
     const val javax_annotation_version = "1.3.2"
     const val os_proto_version = "0.5.0"
     const val jackson_version = "2.12.2"
